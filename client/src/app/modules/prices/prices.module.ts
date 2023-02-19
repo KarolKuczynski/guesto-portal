e9e4da300989ff4908c9e34bce39bc18e34c1b42
Karol Kuczynski <karolkuczynski@o2.pl>
2023-02-19 17:32:29 +0100

    added missing module

--- client/src/app/modules/prices/prices.module.ts
+++ client/src/app/modules/prices/prices.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PricesComponent } from './prices.component';
-
-
+import { PricesRoutingModule } from './prices-routing.module';
 
 @NgModule({
-  declarations: [
-    PricesComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PricesComponent],
+  imports: [CommonModule, PricesRoutingModule],
 })
-export class PricesModule { }
+export class PricesModule {}
