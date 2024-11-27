364e9f24a4dd85d6cb068d8144945e67d9db05d1
KarolKuczynski <karolkuczynski@o2.pl>
2024-11-27 19:10:22 +0100

    added basic nav list menu

--- client/src/app/components/nav-list/nav-list.component.ts
+++ client/src/app/components/nav-list/nav-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Menu } from './model/menu.model';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-list',
   templateUrl: './nav-list.component.html',
   styleUrls: ['./nav-list.component.scss'],
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
 })
 export class NavListComponent {
   menu: Menu[] = [
