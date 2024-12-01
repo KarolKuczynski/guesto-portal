e98c561c82d6cebff4adfeff6d8afc4bc546150a
KarolKuczynski <karolkuczynski@o2.pl>
2024-12-01 17:02:36 +0100

    added basic table layout (to change in the future)

--- client/src/app/modules/rooms/components/rooms-list/rooms-list.component.ts
+++ client/src/app/modules/rooms/components/rooms-list/rooms-list.component.ts
@@ -2,12 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Room } from '../../models/room.model';
 
 @Component({
-    selector: 'app-rooms-list',
-    templateUrl: './rooms-list.component.html',
-    styleUrls: ['./rooms-list.component.scss']
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.scss'],
 })
 export class RoomsListComponent {
   @Input() rooms: Room[] = [];
 
-  columns: string[] = ['id', 'name', 'roomNo', 'adults', 'children'];
+  columns: string[] = ['Id', 'Name', 'RoomNo', 'Adults', 'Children'];
 }
