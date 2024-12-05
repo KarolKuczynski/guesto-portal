f91cd4e0117711ebf96db2ae268703fb38c8b58d
KarolKuczynski <karolkuczynski@o2.pl>
2024-12-05 19:12:18 +0100

    added simple buttons

--- client/src/app/modules/rooms/components/rooms-form/rooms-form.component.ts
+++ client/src/app/modules/rooms/components/rooms-form/rooms-form.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @Component({
-    selector: 'app-rooms-form',
-    templateUrl: './rooms-form.component.html',
-    styleUrls: ['./rooms-form.component.scss']
+  selector: 'app-rooms-form',
+  templateUrl: './rooms-form.component.html',
+  styleUrls: ['./rooms-form.component.scss'],
+  imports: [ButtonComponent],
 })
-export class RoomsFormComponent {
-
-}
+export class RoomsFormComponent {}
