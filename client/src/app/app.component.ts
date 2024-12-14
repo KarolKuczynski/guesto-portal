3ca2c1619cd043e5a600b41636ae0b312c553399
KarolKuczynski <karolkuczynski@o2.pl>
2024-12-14 21:54:23 +0100

    layout changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NavListComponent } from './components/nav-list/nav-list.component';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [NavListComponent, RouterOutlet, NavbarComponent],
+  imports: [NavListComponent, RouterOutlet],
 })
 export class AppComponent {}
