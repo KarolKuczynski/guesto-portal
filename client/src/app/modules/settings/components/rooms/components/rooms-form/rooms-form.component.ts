dc080c56f20ae4c3d40835590d366eb025e8fd4f
KarolKuczynski <karolkuczynski@o2.pl>
2024-12-16 19:17:06 +0100

    small adjustments

--- client/src/app/modules/settings/components/rooms/components/rooms-form/rooms-form.component.ts
+++ client/src/app/modules/settings/components/rooms/components/rooms-form/rooms-form.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @Component({
   selector: 'app-rooms-form',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './rooms-form.component.html',
-  styleUrl: './rooms-form.component.scss'
+  styleUrl: './rooms-form.component.scss',
 })
-export class RoomsFormComponent {
-
-}
+export class RoomsFormComponent {}
