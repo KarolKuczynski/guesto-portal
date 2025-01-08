26fab41fff066f843d99a0afc6dbc1eef555d9df
KarolKuczynski <karolkuczynski@o2.pl>
2025-01-08 18:19:17 +0100

    added ngrx store to the project

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,6 +6,7 @@ import {
   withPreloading,
 } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideStore } from '@ngrx/store';
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules))],
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideStore()],
 }).catch((e) => console.error(e));
