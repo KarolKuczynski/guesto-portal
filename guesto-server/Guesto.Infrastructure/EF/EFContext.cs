85c25530fc10bd9661c97dd6487c53fd9217f159
KarolKuczynski <karolkuczynski@o2.pl>
2025-03-15 17:39:11 +0100

    added sql server configuration for ef

--- guesto-server/Guesto.Infrastructure/EF/EFContext.cs
+++ guesto-server/Guesto.Infrastructure/EF/EFContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Guesto.Core.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Guesto.Infrastructure.EF
 {
     public class EFContext : DbContext
     {
+        public DbSet<Room> Rooms { get; set; }
+
+
     }
 }
